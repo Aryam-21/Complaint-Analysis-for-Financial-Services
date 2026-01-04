1825beebebe3f16b7199d764b4bbdb25ff9d3ee2
Aryam-21 <aryamtesfay963@gmail.com>
2026-01-04 18:00:37 +0300

    Building a RAG-Powered Chatbot using gradio UI.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import gradio as gr
 from src.faiss_retriever import ComplaintRetriever, RAGPromptBuilder, RAGGenerator
 
 # Initialize RAG components
-retriever = ComplaintRetriever(faiss_index_path='../vector_store/faiss.index',
-                               metadata_csv_path='../vector_store/chunks_metadata.csv')
+retriever = ComplaintRetriever(faiss_index_path='vector_store/faiss.index',
+                               metadata_csv_path='vector_store/chunks_metadata.csv')
 prompt_builder = RAGPromptBuilder()
 generator = RAGGenerator(model_name="google/flan-t5-small")
 # Core RAG pipeline function
@@ -23,3 +23,39 @@ def answer_question(question):
     sources = "\n\n".join(f"• Product: {c['product_category']} | Complaint ID: {c['complaint_id']}\n{c['chunk_text']}"
         for c in retrieved_chunks)
     return answer, sources
+# Gradio UI
+with gr.Blocks(title="CrediTrust Complaint Intelligence Assistant") as demo:
+    gr.Markdown(
+        """
+        # 💬 CrediTrust Complaint Intelligence Assistant
+        Ask questions about customer complaints across financial products.
+        The answers are generated **only from real complaint data**.
+        """
+    )
+    question_input = gr.Textbox(
+        label='Enter your question',
+        placeholder='Why are customers unhappy with credit cards?',
+        lines=2
+    )
+    ask_button = gr.Button('Ask')
+    clear_button = gr.Button('Clear')
+    answer_output = gr.Textbox(
+        label='AI Generated Answer',
+        lines=6
+    )
+    sources_output = gr.Textbox(
+        label='Sources (Retrieved Complaint Excerpts)',
+        lines=10
+    )
+    ask_button.click(
+        fn=answer_question,
+        inputs=question_input,
+        outputs=[answer_output, sources_output]
+    )
+    clear_button.click(
+        fn=lambda: ("","",""),
+        inputs=None,
+        outputs=[question_input, answer_output, sources_output]
+    )
+    if __name__ == "__main__":
+        demo.launch()
